3fcfe0192f76bd27a3482f6a3f9846f31e69dba4
Timothy Madegwa <timothymadegwa@gmail.com>
2020-07-28 15:49:42 +0300

    made all the services pages fuctional using django

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('investment', views.investment, name='investment'),
+    path('strategy', views.strategy, name='strategy'),
+    path('risk', views.risk, name='risk'),
+    path('capacity', views.capacity, name='capacity'),
     
 ]
