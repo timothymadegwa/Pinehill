76955dfef9bc65e94609ff94203d3eaa79ec7ae1
Timothy Madegwa <timothymadegwa@gmail.com>
2020-12-24 09:14:18 +0300

    added pdf documents for job postings

--- app/models.py
+++ app/models.py
@@ -37,8 +37,9 @@ class Job(models.Model):
     description = models.TextField()
     requirements = models.TextField()
     posted = models.DateField(default=datetime.now)
-    closing = models.DateField()
+    closing = models.DateTimeField()
     is_published = models.BooleanField(default=True)
+    pdf = models.FileField(upload_to='jobs/pdf', null=True)
 
     def __str__(self):
         return self.title
